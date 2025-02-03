7ce509e3ceff8f454c6dcb4e0891a0e6bd15d867
miomelliot <s89652158910@gmail.com>
2025-02-03 17:35:42 +0300

    chore(🔗): Добавить пути init для файлов разных директорий

---
+++ calllogdb/__init__.py
@@ -0,0 +1,7 @@
+""" 
+Файл для создания модуля программы
+"""
+from .calls import Calls
+from .types import Call, Events
+
+__all__ = ["Calls", "Call", "Events"]
---
+++ calllogdb/types/__init__.py
@@ -0,0 +1,7 @@
+""" 
+Файл для создания модуля программы
+"""
+from .call import Call
+from .events import Events
+
+__all__ = ["Call", "Events"]
