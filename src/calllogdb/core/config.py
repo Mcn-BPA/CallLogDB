943433b2fc1ef9cc46e60ea98b5648f4b3c56481
Mio <s89652158910@gmail.com>
2025-02-24 18:28:12 +0300

    feat✨(PG): Добавить правильную ссылку для подключения к PG

--- src/calllogdb/core/config.py
+++ src/calllogdb/core/config.py
@@ -6,22 +6,33 @@ from dotenv import load_dotenv
 # Загружаем переменные окружения из .env
 load_dotenv()
 
-DB_URL = "sqlite:///db/database.db"
-
 
 @dataclass(frozen=True)
 class Config:
     # GET
     url: str = field(default_factory=lambda: os.getenv("CALL_LOG_URL", ""))
     token: str = field(default_factory=lambda: os.getenv("TOKEN", ""))
+
     # DB
     host: str = field(default_factory=lambda: os.getenv("DB_HOST", "localhost"))
-    port: int = field(default_factory=lambda: int(os.getenv("DB_PORT", 8080)))
+    port: int = field(default_factory=lambda: int(os.getenv("DB_PORT", 5432)))
     user: str = field(default_factory=lambda: os.getenv("DB_USER", "root"))
     password: str = field(default_factory=lambda: os.getenv("DB_PASSWORD", "root"))
+    database: str = field(default_factory=lambda: os.getenv("DB_NAME", "database"))
+
     # LOG
     log_level: str = field(default_factory=lambda: os.getenv("LOG_LEVEL", "INFO"))
 
+    @property
+    def db_url(self) -> str:
+        """
+        Формирует строку подключения к базе данных.
+        """
+        return f"postgresql+psycopg://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
 
-# Экземпляр настроек, который можно использовать по умолчанию
+
+# Экземпляр конфигурации
 config = Config()
+
+# Теперь можно получить `DB_URL` через `config.db_url`
+DB_URL = config.db_url
