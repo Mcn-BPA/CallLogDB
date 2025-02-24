302938a4449aa2bf63e06b5fa6d7aa3b8b75fdd4
Mio <s89652158910@gmail.com>
2025-02-24 12:39:45 +0300

    feat✨(__name__): Изменить имя пакета c `call_log` на `calllog`

--- src/calllogdb/__init__.py
+++ src/calllogdb/__init__.py
@@ -8,14 +8,14 @@ CallLogDB – библиотека для работы с call_log.
 __version__ = "0.1.0"
 
 from .api import APIClient
-from .call_log import CallLog
+from .calllog import calllog
 from .core import Config, config
 from .db import Database
 from .types import Call, Calls, EventBase
 from .utils import parse_datetime, parse_timedelta_seconds
 
 __all__ = [
-    "CallLog",
+    "calllog",
     "APIClient",
     "Call",
     "Calls",
