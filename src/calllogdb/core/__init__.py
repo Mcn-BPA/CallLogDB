b09f66fda0e1bd2fbfd0a958a5a5421b26ca82fe
Mio <s89652158910@gmail.com>
2025-03-05 09:55:21 +0300

    feat✨(exception): Добавить проверку кредов и вызов ошибки

--- src/calllogdb/core/__init__.py
+++ src/calllogdb/core/__init__.py
@@ -2,6 +2,9 @@
 Файл для создания модуля программы
 """
 
-from .config import DB_URL, Config, config
+from .config import Config
+
+config = Config()
+DB_URL = config.db_url
 
 __all__ = ["Config", "config", "DB_URL"]
